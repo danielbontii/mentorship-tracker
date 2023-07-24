7539c29956528ad5a9fc38ca1f843b7bb0fe42b3
danielbontii <bontiidaniel97@gmail.com>
2023-07-24 17:33:03 +0000

    feat: create roles and permissions schema

---
+++ src/main/resources/db/migration/V1__create_roles_and_permission_schemas.sql
@@ -0,0 +1,52 @@
+CREATE TABLE roles
+(
+    /*
+        Todo:
+        1. Do not allow nulls for creation and updating timestamps
+        2. Reference users table for timestamps when users table is ready
+    */
+
+    id              UUID PRIMARY KEY        DEFAULT uuid_generate_v4(),
+    name            VARCHAR UNIQUE NOT NULL,
+    description     VARCHAR,
+
+    created_by      UUID,
+    created_at      TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated_by UUID,
+    last_updated_at TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_by      UUID,
+    deleted_at      TIMESTAMP
+
+
+);
+
+CREATE TABLE permissions
+(
+    /*
+        Todo:
+        1. Do not allow nulls for creation and updating timestamps
+        2. Reference users table for timestamps when users table is ready
+    */
+
+    id              UUID PRIMARY KEY        DEFAULT uuid_generate_v4(),
+    name            VARCHAR UNIQUE NOT NULL,
+
+    created_by      UUID,
+    created_at      TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_updated_by UUID,
+    last_updated_at TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_by      UUID,
+    deleted_at      TIMESTAMP
+
+);
+
+CREATE TABLE roles_permissions
+(
+
+    id            UUID DEFAULT uuid_generate_v4(),
+    role_id       UUID NOT NULL REFERENCES roles (id),
+    permission_id UUID NOT NULL REFERENCES permissions (id),
+
+    PRIMARY KEY (role_id, permission_id)
+
+);
