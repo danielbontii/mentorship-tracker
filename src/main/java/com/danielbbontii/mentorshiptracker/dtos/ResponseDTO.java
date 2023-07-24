30e5ef6ce28d99ef028a9506b24a12df01197575
danielbontii <bontiidaniel97@gmail.com>
2023-07-24 22:17:16 +0000

    feat: create role dto

---
+++ src/main/java/com/danielbbontii/mentorshiptracker/dtos/ResponseDTO.java
@@ -0,0 +1,16 @@
+package com.danielbbontii.mentorshiptracker.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseDTO {
+
+    private String status;
+    private Object data;
+}
---
+++ src/main/java/com/danielbbontii/mentorshiptracker/dtos/RoleDTO.java
@@ -0,0 +1,38 @@
+package com.danielbbontii.mentorshiptracker.dtos;
+
+
+import com.danielbbontii.mentorshiptracker.models.Permission;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RoleDTO {
+
+    @NotBlank(message = "role name must not be empty")
+    private String name;
+
+    private String description;
+
+    private Set<@NotNull Permission> permissions;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RoleDTO roleDTO = (RoleDTO) o;
+        return Objects.equals(name, roleDTO.name) && Objects.equals(description, roleDTO.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
+}
