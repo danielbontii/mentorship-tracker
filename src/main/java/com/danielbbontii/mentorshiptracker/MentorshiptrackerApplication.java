31348920eac024d0a1213326d8f3b90f0abff22a
danielbontii <bontiidaniel97@gmail.com>
2023-07-24 10:49:13 +0000

    chore: add project starter files

---
+++ src/main/java/com/danielbbontii/mentorshiptracker/MentorshiptrackerApplication.java
@@ -0,0 +1,13 @@
+package com.danielbbontii.mentorshiptracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorshiptrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentorshiptrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielbbontii/mentorshiptracker/MentorshiptrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielbbontii.mentorshiptracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentorshiptrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
